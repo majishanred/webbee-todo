893b3ca77bcbe50626a2d9c12cd7a965632d19b3
Matvey Kalashnikov <kalashnikov_matveychik@mail.ru>
2024-06-23 13:46:05 +0200

    refactor(TodoList): Добавил к onAdd возможность обрабатывать массив элементов

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import TodoListElem from './components/TodoItem';
 import TodoInputForm from './components/TodoInputForm';
 import { useCallback, useMemo, useState } from 'react';
-import { v1 as uuidv1 } from 'uuid';
+import { v1 as uuidv1, v1 } from 'uuid';
 import { ITodoItemBody, ITodoItem } from './interfaces/ITodoItem';
 import { StyledBackground } from './styled_components/Background.styled';
 import { StyledWrapper } from './styled_components/Wrapper.styled';
@@ -28,10 +28,20 @@ function App() {
     [tasks, filterInput],
   );
 
-  const onAdd = useCallback((newTodoItem: ITodoItemBody) => {
-    if (!newTodoItem.task) return;
-    const newTodo = { ...newTodoItem, id: uuidv1() };
-    setTasks((tasks) => [...tasks, newTodo]);
+  const onAdd = useCallback((newTodo: ITodoItemBody | ITodoItemBody[]) => {
+    if (newTodo instanceof Array) {
+      const newTodos = newTodo
+        .filter((elem) => elem.task)
+        .map((elem) => {
+          return { ...elem, id: uuidv1() };
+        });
+
+      setTasks((tasks) => [...tasks, ...newTodos]);
+    } else {
+      if (!newTodo.task) return;
+      const elemToAdd = { ...newTodo, id: v1() };
+      setTasks((tasks) => [...tasks, elemToAdd]);
+    }
   }, []);
 
   const onUpdate = useCallback((todoItem: ITodoItem) => {
@@ -47,13 +57,6 @@ function App() {
 
   const onDelete = useCallback((todoItem: ITodoItem) => {
     setTasks((tasks) => tasks.filter((elem) => elem.id !== todoItem.id));
-  }, []);
-
-  const onBatchAdd = useCallback((newTodoItems: ITodoItemBody[]) => {
-    const newTodos = newTodoItems.map((todo) => {
-      return { ...todo, id: uuidv1() };
-    });
-    setTasks((tasks) => [...tasks, ...newTodos]);
   }, []);
 
   return (
